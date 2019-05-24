4365e7e2690ce92b2d096b31da2c0ae6f429dc71
cyz <caiyangzhi123@163.com>
2019-05-24 18:02:06 +0800

    提交

---
+++ pages/index/login/login.vue
@@ -0,0 +1,44 @@
+<template>
+	<view></view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.action-row {
+	    display: flex;
+	    flex-direction: row;
+	    justify-content: center;
+	}
+	
+	.action-row navigator {
+	    color: #007aff;
+	    padding: 0 20upx;
+	}
+	
+	.oauth-row {
+	    display: flex;
+	    flex-direction: row;
+	    justify-content: center;
+	    position: absolute;
+	    top: 0;
+	    left: 0;
+	    width: 100%;
+	}
+	
+	.oauth-image {
+	    width: 100upx;
+	    height: 100upx;
+	    border: 1upx solid #dddddd;
+	    border-radius: 100upx;
+	    margin: 0 40upx;
+	    background-color: #ffffff;
+	}
+	
+	.oauth-image image {
+	    width: 60upx;
+	    height: 60upx;
+	    margin: 20upx;
+	}
+</style>
