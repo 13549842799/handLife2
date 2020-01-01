2038b777c92001f8b1d36930c704836ac6b937e6
NoNameHero <caiyangzhi123@163.com>
2020-01-01 22:51:52 +0800

    提交

--- pages/article/diary/daryTest.vue
+++ pages/article/diary/daryTest.vue
@@ -1,16 +1,26 @@
 <template>
     <view class="container">
-		<web-view src="/hybrid/html/DiaryEditor.html"></web-view>
+		<web-view :src="'/hybrid/html/DiaryEditor.html' + getIdParam"></web-view>
     </view>
 </template>
 
 <script>
     export default {
         data() {
-			return {}
+			return {
+				id: null
+			}
         },
-        methods: {
-        }
+		onLoad(option) {
+			if (option.id) {
+				this.id = option.id
+			}
+		},
+		computed: {
+			getIdParam() {
+				return this.id !== null ? '?id=' + this.id : ''
+			}
+		}
     }
 </script>
 
